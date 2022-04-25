e24dd86bcd9b4181d1bacc58a371342802aa521c
gojay <gao.jay@foxmail.com>
2022-04-25 21:25:45 +0800

    update count_norm.py

--- utils/count_norm.py
+++ utils/count_norm.py
@@ -17,7 +17,7 @@ from torch.utils.data import DataLoader
 
 
 def dataset(data='mnist', dataset=None):
-    """MNIST: Gray, CIFAR10: RGB."""
+    """MNIST: Gray datasets; CIFAR10: RGB datasets."""
     if data == 'mnist':
         dataset = datasets.MNIST("../data", download=True,
                         train=True, transform=transforms.ToTensor())
@@ -25,21 +25,21 @@ def dataset(data='mnist', dataset=None):
         dataset = datasets.CIFAR10("../data", download=True,
                         train=True, transform=transforms.ToTensor())
     dataloader = DataLoader(dataset, shuffle=False, num_workers=os.cpu_count())
-    N_CHANNELS = 3
+    N_CHANNELS = 1 if data == 'mnist' else 3
     mean, std = torch.zeros(1), torch.zeros(1)
 
     for inputs, _labels in tqdm(dataloader):
         for i in range(N_CHANNELS):
             mean[i] += inputs[:,i,:,:].mean()
             std[i] += inputs[:,i,:,:].std()
-    mean.div_(len(dataset))
-    std.div_(len(dataset))
-    print(mean, std)  # tensor([0.1307]) tensor([0.3015])
+    mean = mean.div_(len(dataset)).numpy().tolist()
+    std = std.div_(len(dataset)).numpy().tolist()
+    print(mean, std)  # [0.131] [0.302]
 
 
 def voc():
-    """RGB dataset"""
-    means, stds = [], []
+    """RGB datasets."""
+    mean, std = [], []
     img_list = []
     img_h, img_w = 37, 50
     N_CHANNELS = 3
@@ -57,25 +57,26 @@ def voc():
 
     for i in range(N_CHANNELS):
         pixels = imgs[:, :, i, :].ravel()  # [WHN]
-        means.append(np.mean(pixels))
-        stds.append(np.std(pixels))
+        mean.append(np.mean(pixels))
+        std.append(np.std(pixels))
 
     # BGR --> RGB
-    means.reverse()  # [0.452, 0.431, 0.399]
-    stds.reverse()  # [0.272, 0.269, 0.281]
-    print(means, stds)
+    mean.reverse()  # [0.452, 0.431, 0.399]
+    std.reverse()  # [0.272, 0.269, 0.281]
+    print(mean, std)
 
 
 def imagenet():
-    means = [0.485, 0.456, 0.406]
-    stds = [0.229, 0.224, 0.225]
-    print(means, stds)
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+    print(mean, std)
 
 
 def coco():
-    means = [0.471, 0.448, 0.408]
-    stds = [0.234, 0.239, 0.242]
-    print(means, stds)
+    mean = [0.471, 0.448, 0.408]
+    std = [0.234, 0.239, 0.242]
+    print(mean, std)
+
 
 if __name__ == '__main__':
-    voc()
+    dataset('mnist')
